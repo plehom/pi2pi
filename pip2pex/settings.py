f26e3885ccd8e6d6f53ec4b7ca7acb78de7e77e9
Mirza Pleho <95420439+plehom@users.noreply.github.com>
2025-05-30 20:15:46 +0200

    Update settings.py

--- pip2pex/settings.py
+++ pip2pex/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-86dg^9dswa26&y)zp1e40e$q-h&l$jbbvf7o%l)xsl@)=&+w)+
 DEBUG = True
 
 ALLOWED_HOSTS = ['pi2pi.onrender.com']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://pi2pi.onrender.com'
+]
 
 # Application definition
 
